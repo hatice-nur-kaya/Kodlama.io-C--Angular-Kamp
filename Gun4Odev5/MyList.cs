ff6d267e3d7dc074472d0ec075f48f440e98c029
haticenur.4455 <95941861+HatEla-hub@users.noreply.github.com>
2022-03-11 22:04:07 +0300

    Gun4Odev5

---
+++ Gun4Odev5/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Gun4Odev5
+{
+   
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ Gun4Odev5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gun4Odev5
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> ogrenciler = new MyList<string>();
+            ogrenciler.Add("Hatice Nur KAYA");
+            ogrenciler.Add("HNK");
+
+        }
+    }
+}
